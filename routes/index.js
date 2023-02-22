6045f18d7f8d2d29c714a1ce145f1877ca6eaef0
JoshuaHerring <jdherring2222@gmail.com>
2023-02-22 16:48:02 -0700

    autho0 works on local host, start tests for render

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,33 @@
 const routes = require("express").Router();
 
-routes.use("/bosses", require("./bosses"));
-routes.use("/items", require("./items.js"));
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:8080',
+  clientID: '63HKz9dd2Sr6w5VfP45rdUCHNnPgCRz0',
+  issuerBaseURL: 'https://dev-wujcaylqmgympizc.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+routes.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+routes.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// sends profile if authorized
+
+routes.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
+routes.use("/bosses", requiresAuth(), require("./bosses"));
+routes.use("/items", requiresAuth(), require("./items.js"));
 
 module.exports = routes;
